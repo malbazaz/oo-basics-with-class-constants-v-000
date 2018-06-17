b407efb83c4173f255bdf878038bc7529204dd06
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-17 02:49:29 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,7 +12,13 @@ GENRES = []
     puts "Flipping the page...wow, you read fast!"
   end
 
-
+def genre=(genre)
+  @genre = genre
+if GENRES.include?(genre)
+  nil 
+else 
+  GENRES << genre 
+end 
 
 
 
